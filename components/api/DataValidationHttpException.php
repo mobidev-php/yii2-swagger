e1073930003125cde90bd0d4b77c4ccd2459ca66
Oleksii Kyselov (Kharkiv) <o.kyselov@mobidev.biz>
2015-10-27 12:22:02 +0200

    ErrorValidation exception / bugfixes/ improvements

---
+++ components/api/DataValidationHttpException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: olexii
+ * Date: 10/27/15
+ * Time: 11:38 AM
+ */
+
+namespace vendor\mobidev\swagger\components\api;
+
+
+class DataValidationHttpException
+{
+
+}
